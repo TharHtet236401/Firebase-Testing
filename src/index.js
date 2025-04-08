149466d30528f9dd509e279a15aa92562b0c0212
TharHtet236401 <tharhteta3@gmail.com>
2025-04-08 15:02:08 +0700

    Initialize Firebase in the project and update package dependencies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello World')
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDYrvwspMHB4dHPOP0uQpN_-jkiEVD7nqk",
+    authDomain: "fir-testing-ca024.firebaseapp.com",
+    projectId: "fir-testing-ca024",
+    storageBucket: "fir-testing-ca024.firebasestorage.app",
+    messagingSenderId: "601033842252",
+    appId: "1:601033842252:web:0cbd43482607bc2e3f9ee9"
+  };
+
+ initializeApp(firebaseConfig);
+
+ console.log('Firebase initialized');
